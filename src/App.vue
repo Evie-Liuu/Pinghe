760dbba7a8901fd5022f90955524bc663a3041e1
Evie <kccmewal@yahoo.com>
2025-09-08 22:31:18 +0800

    第一次上傳專案

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <nav>
+    <router-link to="/">首頁</router-link> |
+    <router-link to="/about">關於</router-link> |
+    <router-link to="/actions">行動</router-link>
+  </nav>
+  <router-view></router-view>
+</template>
+
+<script setup>
+</script>
+
+
+<style scoped>
+</style>
