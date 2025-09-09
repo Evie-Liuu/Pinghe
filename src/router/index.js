23fdb214278caaa0b9938de8c7e31dc62115fa8e
Evie <kccmewal@yahoo.com>
2025-09-09 23:13:38 +0800

    部屬至docs

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/views/Home.vue";
-import About from "@/views/About.vue";
-import Actions from "@/views/Actions.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
-  { path: "/about", name: "About", component: About },
-  { path: "/actions", name: "Actions", component: Actions },
-  // { path: "/sdgs", name: "SDGs", component: () => import("@/views/SDGs.vue") },
+  { path: "/about", name: "About", component: () => import("@/views/About.vue") },
+  { path: "/actions", name: "Actions", component: () => import("@/views/Actions.vue") },
+  { path: "/sdgs", name: "SDGs", component: () => import("@/views/SDGs.vue") },
 //   { path: "/story", name: "Story", component: () => import("@/views/Story.vue") },
 //   {
 //     path: "/story/:id",
