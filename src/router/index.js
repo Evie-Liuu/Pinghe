245f585dd159d51c2fb56fc6dffaf15e8df172ef
Evie <kccmewal@yahoo.com>
2025-09-09 23:37:03 +0800

    部屬至docs

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/about", name: "About", component: () => import("@/views/About.vue") },
   { path: "/actions", name: "Actions", component: () => import("@/views/Actions.vue") },
-  { path: "/sdgs", name: "SDGs", component: () => import("@/views/SDGs.vue") },
+  { path: "/sdgs", name: "SDGs", component: () => import("@/views/SDGs.vue") }
 //   { path: "/story", name: "Story", component: () => import("@/views/Story.vue") },
 //   {
 //     path: "/story/:id",
