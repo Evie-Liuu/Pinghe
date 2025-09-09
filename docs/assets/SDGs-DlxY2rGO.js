bc631e4bea865e57199a62d6d12ca1bacdfaa04d
Evie <kccmewal@yahoo.com>
2025-09-09 23:13:10 +0800

    deploy: update GitHub Pages

---
+++ docs/assets/SDGs-DlxY2rGO.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c,o}from"./index-CqfX0A5_.js";const r={};function t(n,s){return o(),c("h1",null,"SDGs")}const f=e(r,[["render",t]]);export{f as default};
