09fb2a3eace5b5399d26b6ed72eec8574de362c6
Evie <cone306915@gmail.com>
2025-09-10 09:31:44 +0800

    關於

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/about">關於</router-link> | 
-    <router-link to="/actions">行動</router-link>
+  <nav class="absolute z-10">
+    <router-link to="/about">關於</router-link> |
+    <router-link to="/actions">行動</router-link> |
+    <router-link to="/sdgs">成果</router-link>
   </nav>
   <router-view></router-view>
 </template>
